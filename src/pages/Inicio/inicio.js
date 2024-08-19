94efd318ece20d24bcf77156d1192f1b7c2b5bad
Valentin <longovalentin@gmail.com>
2024-08-18 22:42:00 -0300

    Se agrega nuevo grafico en inicio

--- src/pages/Inicio/inicio.js
+++ src/pages/Inicio/inicio.js
@@ -1,5 +1,5 @@
 import './inicio.css';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
@@ -11,12 +11,15 @@ function Inicio() {
   const [preparacionCount, setPreparacionCount] = useState(0);
   const [finalizadoCount, setFinalizadoCount] = useState(0);
   const [articulosRepetidos, setArticulosRepetidos] = useState([]);
+  const [pedidosPorDia, setPedidosPorDia] = useState({ labels: [], values: [] });
 
-  useEffect(() => {
+  const fetchPedidos = useCallback(() => {
     fetch(`${process.env.REACT_APP_API_URL}pedidos`)
       .then(response => response.json())
       .then(data => {
         const pedidos = data.data;
+
+        // Contar los pedidos por estado
         const pendiente = pedidos.filter(pedido => Number(pedido.ped_estado) === 1).length;
         const preparacion = pedidos.filter(pedido => Number(pedido.ped_estado) === 2).length;
         const finalizado = pedidos.filter(pedido => Number(pedido.ped_estado) === 3).length;
@@ -24,17 +27,34 @@ function Inicio() {
         setPendienteCount(pendiente);
         setPreparacionCount(preparacion);
         setFinalizadoCount(finalizado);
-      });
 
-    fetch(`${process.env.REACT_APP_API_URL}chart/marca/repeticiones`)
-      .then(response => response.json())
-      .then(data => {
-        setArticulosRepetidos(data.data);
-        console.log(data.data);
+        // Procesar los datos para el gráfico de repeticiones por marca
+        fetch(`${process.env.REACT_APP_API_URL}chart/marca/repeticiones`)
+          .then(response => response.json())
+          .then(data => {
+            setArticulosRepetidos(data.data);
+          });
+
+        // Procesar los datos para el gráfico de pedidos por día
+        const pedidosPorDia = pedidos.reduce((acc, pedido) => {
+          const fecha = new Date(pedido.ped_fecha).toISOString().split('T')[0]; // Formato YYYY-MM-DD
+          if (!acc[fecha]) {
+            acc[fecha] = 0;
+          }
+          acc[fecha]++;
+          return acc;
+        }, {});
+        const labels = Object.keys(pedidosPorDia);
+        const values = Object.values(pedidosPorDia);
+
+        setPedidosPorDia({ labels, values });
       });
   }, []);
 
-  // Configuración de los datos para el gráfico de barras
+  useEffect(() => {
+    fetchPedidos();
+  }, [fetchPedidos]); // Usa fetchPedidos como dependencia
+
   const chartData = {
     labels: articulosRepetidos.map(item => item.mar_descripcion),
     datasets: [
@@ -48,8 +68,21 @@ function Inicio() {
     ],
   };
 
+  const pedidosPorDiaData = {
+    labels: pedidosPorDia.labels,
+    datasets: [
+      {
+        label: 'Pedidos por Día',
+        data: pedidosPorDia.values,
+        backgroundColor: 'rgba(153, 102, 255, 0.6)',
+        borderColor: 'rgba(153, 102, 255, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
-    <div className='todo'>
+    <div className='inicioContainer'>
       <div className="statusContainer">
         <div className="statusBox pendiente">
           <h2>Pendientes</h2>
@@ -65,8 +98,14 @@ function Inicio() {
         </div>
       </div>
       <div className="chartContainer">
-        <h2>Repeticiones por Marca</h2>
-        <Bar data={chartData} />
+        <div>
+          <h2>Repeticiones por Marca</h2>
+          <Bar data={chartData} />
+        </div>
+        <div>
+          <h2>Pedidos por Día</h2>
+          <Bar data={pedidosPorDiaData} />
+        </div>
       </div>
     </div>
   );
